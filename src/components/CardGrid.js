7a258ec26d85cb611e8a8621f9eedb6e3b86607c
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2021-01-02 14:25:40 -0500

    implement pagination for explore page

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -5,17 +5,16 @@ import {useQuery} from '@apollo/client';
 import StoreItems from '../queries/StoreItems.graphql';
 import BetaCard from './BetaCard';
 import useCardGridStyles from './styles/CardGridStyles';
-import {applyFilters} from '../utils/HelperMethods';
 
-const CardGrid = ({filters}) => {
+const CardGrid = ({itemsQueryInfo: {page, filters}}) => {
   console.log(filters)
   const classes = useCardGridStyles();
   const [itemsAdded, setItemsAdded] = useState(0);
 
   const {data: storeItemsData} = useQuery(StoreItems, {
-    // apollo needs a variable to call query again
     variables: {
       input: {
+        page,
         filters
       }
     }
@@ -31,7 +30,6 @@ const CardGrid = ({filters}) => {
             container
             spacing={4}
             className={classes.gridContainer}
-            justify="center"
         >
           <Grid item xs={12} sm={6} md={4} lg={3}>
             <AddCard incrementItemsAdded={incrementItemsAdded} />
--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Button as MuiButton, makeStyles } from "@material-ui/core";
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/purple';
+import {gradientColor} from "../constants";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -11,15 +14,19 @@ const useStyles = makeStyles(theme => ({
     label: {
         textTransform: 'none'
     }
-}))
+}));
+
+const redTheme = createMuiTheme({ palette: { primary: {mainGradient: gradientColor, main: '#ff4400'}} });
 
 export default function Button(props) {
 
-    const { text, size, color, variant, onClick, ...other } = props
+    const { text, size, color, variant, onClick, ...other } = props;
     const classes = useStyles();
 
     return (
+      <MuiThemeProvider theme={redTheme}>
         <MuiButton
+            style={{ background: redTheme.palette.primary.mainGradient }}
             variant={variant || "contained"}
             size={size || "large"}
             color={color || "primary"}
@@ -28,5 +35,6 @@ export default function Button(props) {
             classes={{ root: props.filterButton?classes.filter: classes.root, label: classes.label }}>
             {text}
         </MuiButton>
+      </MuiThemeProvider>
     )
 }
