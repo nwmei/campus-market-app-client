5ec56f3b8ce68b32c4dd55e46e3d856f2549b4c4
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2021-01-07 21:52:37 -0500

    Add configuration of webpack and dependencies to fix bundle code

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
       },
       {
         test: /\.css$/i,
